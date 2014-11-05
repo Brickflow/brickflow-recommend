889aefd2250b10c3a976e774d38ff8fb79a42ee1
Laci Szűcs <ifrozen@gmail.com>
2014-11-05 11:16:17 +0100

    used partials in register action

--- src/actions/register.js
+++ src/actions/register.js
@@ -7,34 +7,30 @@ var InteractionCtr = require('brickflow-common/controller/interactionCtr'),
 
   module.exports = function register(user, callback) {
     async.series({
-      follows: function(cb) {
-        asyncParseFollows(user, function(follows, done) {
-          InteractionCtr.bulkAdd(_.map(follows, function(follow) {
-            return {
-              action: 'follow.blog.initial',
-              blogName: user.tumblrUsername,
-              entityId: follow.name,
-              createdAt: new Date
-            };
-          }), {}, _.noop);
-          done();
-        }, cb);
-
-      },
-      posts: function (cb) {
-        asyncParsePosts(user.tumblrUsername, {reblog_info: true}, function (posts, done) {
-          InteractionCtr.bulkAdd(_.map(posts, function (post) {
-            return {
-              action: 'share.brick.initial',
-              blogName: post.blog_name,
-              entityId: 'tu' + post.id,
-              hashtags: post.tags,
-              originallyPostedBy: post.reblogged_root_name,
-              createdAt: new Date
-            };
-          }));
-          done();
-        }, cb);
-      }
+      follows: _.partial(asyncParseFollows, user, function(follows, done) {
+        InteractionCtr.bulkAdd(_.map(follows, function(follow) {
+          return {
+            action: 'follow.blog.initial',
+            blogName: user.tumblrUsername,
+            entityId: follow.name,
+            createdAt: new Date
+          };
+        }), {}, _.noop);
+        done();
+      }),
+      posts: _.partial(asyncParsePosts, user.tumblrUsername,
+          { reblog_info: true }, function (posts, done) {
+        InteractionCtr.bulkAdd(_.map(posts, function (post) {
+          return {
+            action: 'share.brick.initial',
+            blogName: post.blog_name,
+            entityId: 'tu' + post.id,
+            hashtags: post.tags,
+            originallyPostedBy: post.reblogged_root_name,
+            createdAt: new Date
+          };
+        }));
+        done();
+      })
     }, callback);
 };
